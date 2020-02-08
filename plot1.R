7db5fd0492399b20c0437d80c51e7ddaaf920fe6
Chesalov63 <vchesalov@gmail.com>
2020-02-09 01:53:11 +0400

    plot1

--- plot1.R
+++ plot1.R
@@ -14,7 +14,7 @@ powerDT <- fread(filename, sep = ";", na.strings = "?", skip = "1/2/2007",
                  nrows=2880, col.names = colnames(headerDT))
 
 ## Construct the plot and save it to a PNG file (480x480 by default)
-png(".//data//plot1.png")
+png("plot1.png")
 hist(powerDT$Global_active_power, col = "red", main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)")
 dev.off()
