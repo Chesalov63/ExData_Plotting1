351d1b09907b19cedda4376c1043b3647b858177
Chesalov63 <vchesalov@gmail.com>
2020-02-09 16:22:43 +0400

    plot4

---
+++ plot4.R
@@ -0,0 +1,41 @@
+## Start with downloading and unzipping UCI file
+filename <- "exdata_data_household_power_consumption.zip"
+if(!file.exists(filename)) {
+        download.file(
+                "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip", filename)
+        }
+unzip(filename)
+
+## Read the data from dates 2007-02-01 and 2007-02-02 (2880 records)
+library(data.table)
+library(dplyr)
+filename <- "household_power_consumption.txt"
+headerDT <- fread(filename, sep = ";", na.strings = "?", nrows=0)
+powerDT <- fread(filename, sep = ";", na.strings = "?", skip = "1/2/2007", 
+                 nrows=2880, col.names = colnames(headerDT))
+## Remove the original file, it's too big to keep it unzipped
+file.remove(filename)
+
+## Set locale in case it differs from North-American usage
+Sys.setlocale("LC_TIME", "C")
+## Create new column, because we need Date and Time in POSIXct format
+powerDT <- mutate(powerDT, datetime = as.POSIXct(strptime(paste(Date, Time), 
+                                                          "%d/%m/%Y %H:%M:%S")
+                                                 )
+                  )
+
+## Construct the plot and save it to a PNG file (480x480 by default)
+png("plot4.png")
+par(mfcol = c(2, 2), mar = c(4, 4, 4, 3))
+with(powerDT, plot(Global_active_power ~ datetime, type = "l", 
+                   ylab = "Global Active Power", xlab = ""))
+with(powerDT, plot(Sub_metering_1 ~ datetime, type = "l", 
+                   ylab = "Energy sub metering", xlab = ""))
+with(powerDT, lines(datetime, Sub_metering_2, col = "red"))
+with(powerDT, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", pch = 32, col = c("black", "blue", "red"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lwd = 1, cex = 0.9, bty = "n")
+with(powerDT, plot(Voltage ~ datetime, type = "l"))
+with(powerDT, plot(Global_reactive_power ~ datetime, type = "l"))
+dev.off()
